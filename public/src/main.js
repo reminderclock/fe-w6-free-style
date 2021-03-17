3d1e91ccacc8981225f9e1bf36513a728d8e6148
reminderclock <71510362+reminderclock@users.noreply.github.com>
2021-03-18 01:36:23 +0900

    [Luke] step2 (#34)
    * chore: 정적 markup
    
    * Style: 정적인 부분 style
    
    * feat: json 초안
    
    * chore: json 세트부분 변경
    
    * feat: 대표메뉴 ui
    
    * Feat:: 일반 세트 data 추가
    
    * chore:

---
+++ public/src/main.js
@@ -0,0 +1,53 @@
+const bestMenuContainer = document.querySelector('.best-menu__container');
+const menuContainer = document.querySelector('.menu__container');
+const toogleBtn = document.querySelector('.menu__title__toggle');
+
+const local = "http://localhost"
+const port = 3000;
+const menuPath = "/data/data.json";
+const bestNum = [6,1,19,9,18,11];
+
+function loadMenuData() {
+    return fetch(`${local}:${port}${menuPath}`)
+    .then(response => response.json())
+    .then(json => json.sand__short__set);
+}
+function displayBestMenu(menu) {
+    const bestMenu = menu.filter(e => bestNum.includes(e.menuNum));
+    let eachInfo = bestMenu.map(e => creatBestMenu(e));
+}
+function numToCash(num) {
+    return num.toLocaleString( 'ko-KR', { style: 'currency', currency: 'KRW' } );
+}
+function creatBestMenu(e) {
+    let cost = numToCash(e.cost);
+    bestMenuContainer.innerHTML += `
+    <ul class="best-menu__bundle">
+    <img src="${e.imgurl}" alt="${e.name}" class="best-menu__img" />
+    <li class="menu-name">${e.name}(${e.length})</li>
+    <li class="menu-cost">${cost}원</li>
+    </ul>`;
+}
+
+function creatMenu(e) {
+    let cost = numToCash(e.cost);
+    menuContainer.innerHTML += `
+    <ul class="best-menu__bundle">
+    <img src="${e.imgurl}" alt="${e.name}" class="best-menu__img" />
+    <li class="menu-name">${e.name}(${e.length})</li>
+    <li class="menu-cost">${cost}원</li>
+    </ul>`;
+}
+function displayShortSet(menu) {
+    let a = menu.map(e => creatMenu(e));
+}
+loadMenuData()
+.then((menu) => {
+    displayBestMenu(menu);
+    displayShortSet(menu);
+})
+
+toogleBtn.addEventListener('click', () => {
+    menuContainer.classList.toggle('active')
+  });
+

21ddc1a1eee91c4a41c55681b03cb5f41c9bd166
reminderclock <reminderclock@naver.com>
2021-03-17 18:32:38 +0900

    Feat:: 일반 세트 data 추가

--- public/src/main.js
+++ public/src/main.js
@@ -1,6 +1,8 @@
 const bestMenuContainer = document.querySelector('.best-menu__container');
+const menuContainer = document.querySelector('.menu__container');
+const toogleBtn = document.querySelector('.menu__title__toggle');
 
-
+// nth-child(6)
 const local = "http://localhost"
 const port = 3000;
 const menuPath = "/data/data.json";
@@ -24,12 +26,30 @@ function creatBestMenu(e) {
     bestMenuContainer.innerHTML += `
     <ul class="best-menu__bundle">
     <img src="${e.imgurl}" alt="${e.name}" class="best-menu__img" />
-    <li>${e.name}(${e.length})</li>
-    <li>${cost}원</li>
+    <li class="menu-name">${e.name}(${e.length})</li>
+    <li class="menu-cost">${cost}원</li>
     </ul>`;
+}
+
+function creatMenu(e) {
+    let cost = numToCash(e.cost);
+    menuContainer.innerHTML += `
+    <ul class="best-menu__bundle">
+    <img src="${e.imgurl}" alt="${e.name}" class="best-menu__img" />
+    <li class="menu-name">${e.name}(${e.length})</li>
+    <li class="menu-cost">${cost}원</li>
+    </ul>`;
+}
+function displayShortSet(menu) {
+    let a = menu.map(e => creatMenu(e));
 }
 loadMenuData()
 .then((menu) => {
     displayBestMenu(menu);
+    displayShortSet(menu);
 })
+
+toogleBtn.addEventListener('click', () => {
+    menuContainer.classList.toggle('active')
+  });
 
